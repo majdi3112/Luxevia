a0259d33d309ceb70b1e8e85ffa0649b61df7919
majdi3112 <147162590+majdi3112@users.noreply.github.com>
2026-03-15 04:55:39 +0100

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Luxevia/',
   server: {
-    port: 5173
-  }
-});
-
+    port: 5173,
+  },
+})
